300cf598b21fef1d4e04d40145a6cf208c190012
hyriss02 <113988964+hyriss02@users.noreply.github.com>
2022-11-14 19:31:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6282287219167']
-global.ownernomer = "6282287219167"
-global.premium = ['6282287219167']
+global.owner = ['6281256023873']
+global.ownernomer = "6281256023873"
+global.premium = ['6281256023873']
 global.packname = 'Sticker By'
-global.author = 'Sad-Bot'
+global.author = '༄Riss Botz࿐'
 global.sessionName = 'papah'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -65,9 +65,9 @@ global.myytv = ''
 global.mygc = ""
 
 // Engak Ngaruh, Belum Kepasang
-global.botname = 'SAD BOT'
-global.akulaku = 'Bot By Papah'
-global.ytname = 'YT PAPAH' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+global.botname = '༄Riss Botz࿐'
+global.akulaku = 'Bot By Riss'
+global.ytname = 'Riss ' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
